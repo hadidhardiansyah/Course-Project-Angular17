397115600b5af7b8126a8361d04d484ff55e4230
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2024-02-10 13:43:29 +0700

    Course Project - Making Http Requests

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,28 +8,34 @@ import {Subject} from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Recipe 1',
-      'This is the Pasta Creamy',
-      'https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg',
-      [
-        new Ingredient('Meat', 100),
-        new Ingredient('Pasta', 50)
-      ]
-    ),
-    new Recipe(
-      'Recipe 2',
-      'Hot Noodle with vegetable',
-      'https://www.foodandwine.com/thmb/tAS-x_IC4ss1cb9EdDpsr0UExdM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/bucatini-with-mushroom-ragu-dandelion-greens-and-tarragon-FT-RECIPE0421-3a5f0d29f7264f5e9952d4a3a51f5f58.jpg',
-      [
-        new Ingredient('Noodle', 90),
-        new Ingredient('Sauce', 1)
-      ]
-    )
-  ];
+  // recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Recipe 1',
+  //     'This is the Pasta Creamy',
+  //     'https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg',
+  //     [
+  //       new Ingredient('Meat', 100),
+  //       new Ingredient('Pasta', 50)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Recipe 2',
+  //     'Hot Noodle with vegetable',
+  //     'https://www.foodandwine.com/thmb/tAS-x_IC4ss1cb9EdDpsr0UExdM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/bucatini-with-mushroom-ragu-dandelion-greens-and-tarragon-FT-RECIPE0421-3a5f0d29f7264f5e9952d4a3a51f5f58.jpg',
+  //     [
+  //       new Ingredient('Noodle', 90),
+  //       new Ingredient('Sauce', 1)
+  //     ]
+  //   )
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipes() {
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {RecipeService} from "../recipes/recipe.service";
+import {Recipe} from "../recipes/recipe.model";
+import {map, tap} from "rxjs";
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(
+      'https://course-project-78441-default-rtdb.firebaseio.com/recipes.json',
+      recipes
+    ).subscribe((response) => {
+      console.log(response);
+    })
+  }
+
+  // fetchRecipes() {
+  //   return this.http.get<Recipe[]>(
+  //     'https://course-project-78441-default-rtdb.firebaseio.com/recipes.json'
+  //   ).pipe(
+  //     map(recipes => {
+  //       return recipes.map(recipe => {
+  //         return {
+  //           ...recipe,
+  //           ingredients: recipe.ingredients ? recipe.ingredients : []
+  //         };
+  //       });
+  //     }),
+  //     tap(recipes => {
+  //       this.recipeService.setRecipes(recipes);
+  //     })
+  //   )
+  // }
+
+  fetchRecipes() {
+    return this.http.get<Recipe[]>(
+      'https://course-project-78441-default-rtdb.firebaseio.com/recipes.json'
+    ).pipe(
+      map(recipes => recipes ? recipes.map(recipe => ({ ingredients: [], ...recipe })) : []),
+      tap(recipes => this.recipeService.setRecipes(recipes))
+    );
+  }
+}
